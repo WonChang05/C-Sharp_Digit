cd0d87fed8c8f72ad5a7451bf3a883b601e13840
WonChang05 <50658107+WonChang05@users.noreply.github.com>
2019-06-23 01:22:30 +0800

    Add files via upload

---
+++ digit/digit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(while迴圈練習)算出數字有幾位數
+            string flag = "y";
+            while (flag == "y")
+            {
+                Console.WriteLine("請輸入一個數值:");
+                int temp = Convert.ToInt32(Console.ReadLine());
+                int result = 0;
+
+                while (temp != 0)
+                {
+                    //Console.WriteLine($"{temp}/10 ==> {temp / 10}");
+                    temp = temp / 10;
+                    result = result + 1;
+                }
+                Console.WriteLine($"總共有{result}位數");
+                Console.WriteLine("是否要繼續？y/n");
+                if (Console.ReadLine() == "n")
+                {
+                    flag = "n";
+                }
+            }
+        }
+    }
+}
